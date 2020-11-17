f8084830421f13d69d094dfbd4722ac26346df66
Valendaquino <vdaquino@udesa.edu.ar>
2020-11-17 10:46:03 -0300

    fetch de home

--- js/home.js
+++ js/home.js
@@ -1,10 +1,10 @@
-let media ="movies";
+/* let media ="movies";
 let timeWindow = "week";
 let apiKey = "764e5562e5fed92cb370d453ac0ed8a3";
 let url = `https://api.themoviedb.org/3/trending/${media}/${timeWindow}?api_key=${apiKey}`;
-let tendencias = document.querySelector( '.tendencias')
+let tendencias = document.querySelector( '.tendencias') */
 
-fetch(url)
+fetch('https://api.themoviedb.org/3/trending/movies/week?api_key=764e5562e5fed92cb370d453ac0ed8a3')
     .then(function(respuestas){
         return respuestas.json();
     })
@@ -12,19 +12,14 @@ fetch(url)
         console.log(data);
 
         let info= data.results 
+        let tendencias = document.querySelector( '.tendencias a')
         //let id = info.id
         //console.log(id);
         for (let i=0; i<info.length; i++){
 
-            tendencias.innerHTML = 
+            tendencias.innerHTML += 
                                         ` <a href="detallep.html?id{info[i].id}"><img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="fiebredeladrillo"></a>
                                        
-                                        <a href="detallep.html?id{info[i].id}"><img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}"  alt="hastaelutimo"></a>
-                                       
-                                        <a href="detallep.html?id{info[i].id}"><img  src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="macdonals"></a>
-                                       
-                                        <a href="detallep.html?id{info[i].id}"><img  src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}"  alt="toystory"></a>
-                                       
-                                        <a class="loimposible" href="detallep.html?id{info[i].id}"><img src="img/loimposible.jpeg""https://image.tmdb.org/t/p/w500/${info[i].poster_path}"  alt="loimposible"></a>`
+                                       `
                                 } 
     })
