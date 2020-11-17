506165c1e1e89abf5d06d0879f76a5d6137ae15e
ale-vivone <vivoneaj@gmail.com>
2020-11-17 18:22:55 -0300

    demo de slider, demo de detalle pelicula y buscador.

--- js/detallep.js
+++ js/detallep.js
@@ -1,57 +1,26 @@
-/*let queryString = window.location.search;
+let queryString = window.location.search;
 console.log(queryString);
 let queryObject = new URLSearchParams(queryString)
 let id = queryObject.get ('id');
 console.log(id);
 
-
 let apiKey = "764e5562e5fed92cb370d453ac0ed8a3"
-let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
-fetch(`id`)
+let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+
+let elementos = document.querySelector('.elementos')
+
+fetch(url)
 .then(function (respuestas){
     return respuestas.json()
 })
 
 .then(function(data){
     console.log(data)
-    let poster = document.querySelector('.foto')
-   poster.src = `https://image.tmdb.org/t/p/w500${backdrop_path}`
+    elementos.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${data.poster_path})`
+   
     
 })
-.catch(error => console.log(error))*/
+.catch(error => console.log(error))
 
 
 
-
-
-
-/* let media ="movies";
-let timeWindow = "week";
-let apiKey = "764e5562e5fed92cb370d453ac0ed8a3";
-let url = `https://api.themoviedb.org/3/trending/${media}/${timeWindow}?api_key=${apiKey}`;
-let tendencias = document.querySelector( '.tendencias') */
-
-/* fetch('https://api.themoviedb.org/3/trending/movies/week?api_key=764e5562e5fed92cb370d453ac0ed8a3')
-    .then(function(respuestas){
-        return respuestas.json();
-    })
-    .then (function(data){
-        console.log(data);
-
-        let info= data.results 
-        let pelicula = document.querySelector( '.carousel__elemento ')
-        //let id = info.id
-		//console.log(id);
-		let imagen = document.querySelector ('.pelis ')
-        for (let i=0; i<info.length; i++){
-/* 
-            pelicula.innerHTML += 
-                                        ` <a href="detallep.html?id{info[i].id}"><img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt=""></a>
-                                       
-                                       `
-								}  */
-		/* 	imagen.src= `https://image.tmdb.org/t/p/w500/${info[i].poster_path}`
-			 }
-	})
-	
-	 */ */
--- js/resultadob.js
+++ js/resultadob.js
@@ -6,7 +6,10 @@ console.log(search);
 
 
 let apiKey = "764e5562e5fed92cb370d453ac0ed8a3"
-let url = `https://api.themoviedb.org/3/search/${search}?api_key=764e5562e5fed92cb370d453ac0ed8a3&language=en-US&page=1&include_adult=false`
+let url = `https://api.themoviedb.org/3/search/multi?api_key=764e5562e5fed92cb370d453ac0ed8a3&language=en-US&query=${search}&page=1&include_adult=false`
+
+let resultados = document.querySelector('.resultados')
+
 fetch(url)
 .then(function (respuestas){
     return respuestas.json()
@@ -14,6 +17,16 @@ fetch(url)
 
 .then(function(data){
     console.log(data);
+    let info = data.results;
+
+    for (let i=0; i<info.length; i++){
+        if(info[i].media_type == "tv"){
+            resultados.innerHTML += `<li>Serie: ${info[i].original_name}</li>`
+        } else if (info[i].media_type == "movie"){
+            resultados.innerHTML += `<li>Pelicula: ${info[i].title}</li>`
+        }
+        
+    }
     
 })
 .catch(error => console.log(error))
