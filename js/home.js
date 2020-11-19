c064766852e7de0d0fa3df89c5fc2fc544e7b012
Guido Burzaco <ariel@192.168.1.63>
2020-11-18 22:27:19 -0300

    carousel y fetch de home

--- js/home.js
+++ js/home.js
@@ -1,25 +1,80 @@
-/* let media ="movies";
-let timeWindow = "week";
-let apiKey = "764e5562e5fed92cb370d453ac0ed8a3";
-let url = `https://api.themoviedb.org/3/trending/${media}/${timeWindow}?api_key=${apiKey}`;
-let tendencias = document.querySelector( '.tendencias') */
 
-fetch('https://api.themoviedb.org/3/trending/movies/week?api_key=764e5562e5fed92cb370d453ac0ed8a3')
-    .then(function(respuestas){
-        return respuestas.json();
-    })
-    .then (function(data){
-        console.log(data);
 
-        let info= data.results 
-        let tendencias = document.querySelector( '.tendencias a')
-        //let id = info.id
-        //console.log(id);
-        for (let i=0; i<info.length; i++){
+let slider = document.querySelector('.uk-slider-items')
 
-            tendencias.innerHTML += 
-                                        ` <a href="detallep.html?id{info[i].id}"><img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="fiebredeladrillo"></a>
-                                       
-                                       `
-                                } 
-    })
+
+fetch("https://api.themoviedb.org/3/trending/all/week?api_key=764e5562e5fed92cb370d453ac0ed8a3")
+.then(function (respuestas){
+    return respuestas.json()
+})
+
+.then(function(data){
+    console.log(data.results);
+    let info = data.results;
+    
+    for(let i=0; i<info.length; i++){
+        slider.innerHTML += `<li>
+                                <a href="detallep.html?id=${info[i].id}">
+                                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                                    <div class="uk-position-center uk-panel">
+                                    </div>
+                                </a>
+                            </li>`
+    }
+
+})
+.catch(error => console.log(error));  
+
+
+
+let sliderdos = document.querySelector('.peli')
+fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=764e5562e5fed92cb370d453ac0ed8a3")
+.then(function (respuestas){
+    return respuestas.json()
+})
+
+.then(function(data){
+    console.log(data.results);
+    let infomovie = data.results;
+    
+    for(let i=0; i<infomovie.length; i++){
+        sliderdos.innerHTML += `<ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m"><li>
+                                <a href="detallep.html?id=${infomovie[i].id}">
+                                    <img src="https://image.tmdb.org/t/p/w500${infomovie[i].poster_path}" alt="">
+                                    <div class="uk-position-center uk-panel">
+                                    </div>
+                                </a>
+
+                            </li> 
+              
+           
+                            </ul>` 
+    }
+
+})
+.catch(error => console.log(error)); 
+
+
+
+
+fetch("https://api.themoviedb.org/3/trending/tv/week?api_key=764e5562e5fed92cb370d453ac0ed8a3")
+.then(function (respuestas){
+    return respuestas.json()
+})
+
+.then(function(data){
+    console.log(data.results);
+    let infotv = data.results;
+    
+    for(let i=0; i<infotv.length; i++){
+        slider.innerHTML += `<li>
+                                <a href="detallep.html?id=${infotv[i].id}">
+                                    <img src="https://image.tmdb.org/t/p/w500${infotv[i].poster_path}" alt="">
+                                    <div class="uk-position-center uk-panel">
+                                    </div>
+                                </a>
+                            </li>`
+    }
+
+})
+.catch(error => console.log(error)); 
